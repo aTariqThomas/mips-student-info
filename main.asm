47cad68994bdbc3c4760bd6812728ebf2a7d0935
Gerald Nash <g.nash.dev@gmail.com>
2018-10-15 19:42:37 -0400

    Start printing chars the slow way

--- main.asm
+++ main.asm
@@ -1,9 +1,17 @@
-.data
-  output: .asciiz "hi\n"
 .text
   main:
-    li $v0, 4
-    la $a0, output
+    li $v0, 11
+    la $a0, 71  # G
+    syscall
+    li $a0, 101 # e
+    syscall
+    li $a0, 114 # r
+    syscall
+    li $a0, 97  # a
+    syscall
+    li $a0, 108 # l
+    syscall
+    li $a0, 100 # d
     syscall
     li $v0, 10
     syscall
